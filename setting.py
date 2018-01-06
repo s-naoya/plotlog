61ba05b7ea19eb700ef2b103fd8ada95e2264a97
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-06 23:09:49 +0900

    read user setting file

--- setting.py
+++ setting.py
@@ -3,10 +3,15 @@ import yaml
 
 class Setting:
     default = None
+    setting = None
+    __default_file_path = "default.yml"
+    __setting_file_path = None
 
-    def __init__(self):
-        pass
+    def __init__(self, setting_file_path):
+        self.__setting_file_path = setting_file_path
 
     def configure(self):
-        with open("default.yml") as f:
+        with open(self.__default_file_path) as f:
             self.default = yaml.load(f)
+        with open(self.__setting_file_path) as f:
+            self.setting = yaml.load(f)
--- test_setting.py
+++ test_setting.py
@@ -4,9 +4,14 @@ from setting import Setting
 
 class TestSetting(unittest.TestCase):
     def test_read_default_file(self):
-        setting = Setting()
+        setting = Setting("setting.yml")
         setting.configure()
         self.assertIsNotNone(setting.default)
+
+    def test_read_setting_file(self):
+        setting = Setting("setting.yml")
+        setting.configure()
+        self.assertIsNotNone(setting.setting)
 
 
 if __name__ == '__main__':
