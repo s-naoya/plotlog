a81a89b083d3b6fcfbd9f428b925315e71031ec4
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-07 01:28:59 +0900

    separate setting file user and test

--- test/test_setting.py
+++ test/test_setting.py
@@ -4,17 +4,17 @@ from src.setting import Setting
 
 class TestSetting(unittest.TestCase):
     def test_read_default_file(self):
-        obj = Setting("user.yml")
+        obj = Setting("test/test.yml")
         obj.configure()
         self.assertIsNotNone(obj.default)
 
     def test_read_user_file(self):
-        obj = Setting("user.yml")
+        obj = Setting("test/test.yml")
         obj.configure()
         self.assertIsNotNone(obj.user)
 
     def test_config_setting_dic(self):
-        obj = Setting("user.yml")
+        obj = Setting("test/test.yml")
         obj.configure()
         self.assertNotEqual(obj.setting["log_extension"],
                             obj.default["log_extension"])
