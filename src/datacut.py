c2d48fffdf8aaa349ab3dbec1da923f8d3844237
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-07 15:02:53 +0900

    import log file to dataframe

--- src/datacut.py
+++ src/datacut.py
@@ -1,2 +1,15 @@
+import pandas as pd
+
+
 class DataCut:
-    pass
+    __log_file_path = None
+    df = None
+
+    def __init__(self, log_file_path):
+        self.__log_file_path = log_file_path
+
+    def dispose(self):
+        self.__log_file_path = None
+
+    def import_file(self, header, sep):
+        self.df = pd.read_csv(self.__log_file_path, header=header, sep=sep)
--- test/test_dataCut.py
+++ test/test_dataCut.py
@@ -1,15 +1,27 @@
 import unittest
 
+from src.datacut import DataCut
 from src.setting import Setting
 
 
 class TestDataCut(unittest.TestCase):
     def setUp(self):
-        self.setting = Setting("test/test.yml")
+        self.setting = Setting("default.yml", "../test/test.yml")
         self.setting.configure()
+        self.data = DataCut("../test/log/201712251354.csv")
 
     def tearDown(self):
         self.setting.dispose()
+        self.data.dispose()
+
+    def test_import_logfile(self):
+        self.data.import_file(self.setting.setting["header_row"],
+                              self.setting.setting["log_separate_type"])
+        self.assertIsNotNone(self.data.df)
+
+    def test_import_logfile_no_use_header(self):
+        self.data.import_file(None, self.setting.setting["log_separate_type"])
+        self.assertEqual(self.data.df.columns[2], 2)
 
 
 if __name__ == '__main__':
