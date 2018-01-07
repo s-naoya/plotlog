b3f1b5f47f0d9bf04a4c78433b08e310561bb610
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-07 17:15:34 +0900

    add slice

--- src/datacut.py
+++ src/datacut.py
@@ -1,3 +1,4 @@
+import copy
 import pandas as pd
 
 
@@ -5,33 +6,43 @@ class DataCut:
     df = None
     x_axis = None
 
-    __log_file_path = None
+    __log_file_path = list()
     __x_col = None
 
-    def __init__(self, log_file_path):
-        self.__log_file_path = log_file_path
+    def __init__(self, log_file_path_list):
+        for path in log_file_path_list:
+            self.__log_file_path.append(path)
 
     def dispose(self):
-        self.__log_file_path = None
+        self.__log_file_path.clear()
         self.df = None
 
     def import_file(self, header, sep):
-        self.df = pd.read_csv(self.__log_file_path, header=header, sep=sep)
+        for path in self.__log_file_path:
+            self.df = pd.read_csv(path, header=header, sep=sep)
 
     def set_x_axis(self, x_col):
         self.x_axis = self.df[x_col]
         self.__x_col = x_col
 
-    def slice(self, trig_col, trig_val):
-        arr = self.df.as_matrix()
-        trig_arr = self.df[trig_col].as_matrix()
+    def shift(self, trig_col, trig_val):
+        trig_df = self.df[trig_col]
         idx = [0, None]
-        while abs(float(trig_arr[idx[0]])) < trig_val:
+        while abs(float(trig_df[idx[0]])) < trig_val:
             idx[0] += 1
-        shift_time = arr[idx[0], 0]
-        shift_arr = arr[idx[0]:idx[1], :]
-        shift_arr[:, 0] = shift_arr[:, 0] - shift_time
-        shift_df = pd.DataFrame(shift_arr)
-        shift_df.columns = self.df.columns
-        self.df = shift_df
+        shift_start_time = self.x_axis[idx[0]]
+        shift_df = self.df.iloc[idx[0]:idx[1], :]
+        shift_df[self.__x_col] = shift_df[self.__x_col] - shift_start_time
+        self.df = shift_df.reset_index(drop=True)
+        self.set_x_axis(self.__x_col)
+
+    def slice(self, begin, end):
+        idx = [0, None]
+        while float(self.x_axis[idx[0]]) < begin:
+            idx[0] += 1
+        idx[1] = idx[0]
+        while float(self.x_axis[idx[1]]) <= end:
+            idx[1] += 1
+        slice_df = self.df.iloc[idx[0]:idx[1], :]
+        self.df = slice_df.reset_index(drop=True)
         self.set_x_axis(self.__x_col)
--- test/test_dataCut.py
+++ test/test_dataCut.py
@@ -5,7 +5,7 @@ from src.datacut import DataCut
 
 class TestDataCut(unittest.TestCase):
     def setUp(self):
-        self.data = DataCut("../test/log/201712251354.csv")
+        self.data = DataCut(["../test/log/201712251354.csv"])
 
     def tearDown(self):
         self.data.dispose()
@@ -23,13 +23,21 @@ class TestDataCut(unittest.TestCase):
         self.data.set_x_axis("time")
         self.assertEqual(self.data.x_axis[0], 0.005)
 
-    def test_df_slice(self):
+    def test_df_shift(self):
         self.data.import_file(0, ",")
         self.data.set_x_axis("time")
-        self.data.slice("refCoM_y", 1e-5)
+        self.data.shift("refCoM_y", 1e-5)
         self.assertEqual(self.data.x_axis[0], 0.0)
         self.assertTrue(self.data.df["refCoM_y"][0] < 0.01)
         self.assertTrue(self.data.df["refCoM_y"][50] > 0.001)
+
+    def test_df_slice(self):
+        self.data.import_file(0, ",")
+        self.data.set_x_axis("time")
+        self.data.slice(5.0, 10.0)
+        self.assertEqual(self.data.x_axis[0], 5.0)
+        self.assertEqual(self.data.x_axis.tail(1).values[0], 10.0)
+        self.assertEqual(len(self.data.x_axis), len(self.data.df))
 
 
 if __name__ == '__main__':
