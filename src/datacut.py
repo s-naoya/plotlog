f44364dfe672c2ea04007190717d9a946965fe75
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-07 15:35:52 +0900

    extraction x axis data

--- src/datacut.py
+++ src/datacut.py
@@ -2,14 +2,19 @@ import pandas as pd
 
 
 class DataCut:
-    __log_file_path = None
     df = None
+    x_axis = None
+    __log_file_path = None
 
     def __init__(self, log_file_path):
         self.__log_file_path = log_file_path
 
     def dispose(self):
         self.__log_file_path = None
+        self.df = None
 
     def import_file(self, header, sep):
         self.df = pd.read_csv(self.__log_file_path, header=header, sep=sep)
+
+    def set_x_axis(self, x_col):
+        self.x_axis = self.df[x_col]
--- test/test_dataCut.py
+++ test/test_dataCut.py
@@ -1,27 +1,27 @@
 import unittest
 
 from src.datacut import DataCut
-from src.setting import Setting
 
 
 class TestDataCut(unittest.TestCase):
     def setUp(self):
-        self.setting = Setting("default.yml", "../test/test.yml")
-        self.setting.configure()
         self.data = DataCut("../test/log/201712251354.csv")
 
     def tearDown(self):
-        self.setting.dispose()
         self.data.dispose()
 
     def test_import_logfile(self):
-        self.data.import_file(self.setting.setting["header_row"],
-                              self.setting.setting["log_separate_type"])
+        self.data.import_file(0, ",")
         self.assertIsNotNone(self.data.df)
 
     def test_import_logfile_no_use_header(self):
-        self.data.import_file(None, self.setting.setting["log_separate_type"])
+        self.data.import_file(None, ",")
         self.assertEqual(self.data.df.columns[2], 2)
+
+    def test_df_x_axis(self):
+        self.data.import_file(0, ",")
+        self.data.set_x_axis("time")
+        self.assertEqual(self.data.x_axis[0], 0.005)
 
 
 if __name__ == '__main__':
