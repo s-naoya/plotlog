6ada3e8233af868cfe12b6bfb8ece96a6c152cc4
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-07 23:31:33 +0900

    add plot

--- src/plotgraph.py
+++ src/plotgraph.py
@@ -1,2 +1,32 @@
+import matplotlib.pyplot as plt
+
+
 class PlotGraph:
-    pass
+    def_style = ["-", "--", "-.", ":"]
+    def_num = 0
+
+    def __init__(self):
+        pass
+
+    def plot(self, path, st, gr, x, df):
+        plt.figure(figsize=(st["graph_size"][0]*0.01, st["graph_size"][1]*0.01))
+        plt.xlabel(gr["xlabel"])
+        plt.ylabel(gr["ylabel"])
+        for grp in gr["plot"]:
+            if grp["style"] == "order":
+                grp["style"] = self.def_style[self.def_num]
+                self.def_num = self.def_num + 1 if self.def_num < 3 else 0
+
+            plt.plot(
+                x, df[grp["col"]],
+                label=grp["label"], color=grp["color"],
+                linestyle=grp["style"], linewidth=grp["width"]
+            )
+        plt.xlim(st["xlim"])
+        plt.ylim(gr["ylim"])
+
+        plt.legend(loc=gr["legend"]["loc"],
+                   bbox_to_anchor=gr["legend"]["bbox_to_anchor"],
+                   ncol=gr["legend"]["ncol"])
+        plt.savefig(path)
+        plt.close()
--- test/test_plotGraph.py
+++ test/test_plotGraph.py
@@ -1,8 +1,27 @@
 import unittest
 
+from src.datacut import DataCut
+from src.plotgraph import PlotGraph
+from src.setting import Setting
+
 
 class TestPlotGraph(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.st = Setting("../src/default.yml", "test.yml")
+        self.st.configure()
+        self.data = DataCut(["../test/log/201712251354.csv"])
+        self.data.import_file(0, ",")
+        self.data.set_x_axis("time")
+        self.pg = PlotGraph()
+
+    def tearDown(self):
+        self.data.dispose()
+
+    def test_plot_graph(self):
+        for stg in self.st.graph:
+            path = "graph/test_"+stg["name"]+".png"
+            self.pg.plot(path, self.st.setting, stg,
+                         self.data.x_axis, self.data.df)
 
 
 if __name__ == '__main__':
