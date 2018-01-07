59e0b1835b35d3d716f635749a063368abf0fbe7
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-07 14:06:55 +0900

    add setUp and tearDown

--- test/test_setting.py
+++ test/test_setting.py
@@ -3,39 +3,33 @@ from src.setting import Setting
 
 
 class TestSetting(unittest.TestCase):
-    def test_read_default_file(self):
-        obj = Setting("test/test.yml")
-        obj.configure()
-        self.assertIsNotNone(obj.default)
+    def setUp(self):
+        self.obj = Setting("test/test.yml")
+        self.obj.configure()
 
-    def test_read_user_file(self):
-        obj = Setting("test/test.yml")
-        obj.configure()
-        self.assertIsNotNone(obj.user)
+    def tearDown(self):
+        self.obj.dispose()
 
     def test_config_setting_dic(self):
-        obj = Setting("test/test.yml")
-        obj.configure()
-        self.assertNotEqual(obj.setting["log_extension"],
-                            obj.default["log_extension"])
-        self.assertNotEqual(obj.setting["footprint"]["foot_size"],
-                            obj.default["footprint"]["foot_size"])
-        self.assertEqual(obj.setting["footprint"]["supleg_label"],
-                         obj.default["footprint"]["supleg_label"])
+        self.assertEqual(len(self.obj.graph), 2)
+        self.assertNotEqual(self.obj.setting["log_extension"],
+                            self.obj.default["log_extension"])
+        self.assertNotEqual(self.obj.setting["footprint"]["foot_size"],
+                            self.obj.default["footprint"]["foot_size"])
+        self.assertEqual(self.obj.setting["footprint"]["supleg_label"],
+                         self.obj.default["footprint"]["supleg_label"])
 
     def test_config_graph_dic(self):
-        obj = Setting("test/test.yml")
-        obj.configure()
-        self.assertEqual(len(obj.graph), 2)
-        self.assertNotEqual(obj.graph[0]["xlabel"],
-                            obj.default["graph"][0]["xlabel"])
-        self.assertEqual(obj.graph[0]["xlabel"], "time[s]")
-        self.assertEqual(obj.graph[0]["ylim"],
-                         obj.default["graph"][0]["ylim"])
-        self.assertEqual(len(obj.graph[0]["plot"]), 2)
-        self.assertTrue("color" in obj.graph[0]["plot"][0])
-        self.assertEqual(obj.graph[0]["legend"]["loc"], "best")
-        self.assertIsNone(obj.graph[0]["legend"]["ncol"])
+        self.assertEqual(len(self.obj.graph), 2)
+        self.assertNotEqual(self.obj.graph[0]["xlabel"],
+                            self.obj.default["graph"][0]["xlabel"])
+        self.assertEqual(self.obj.graph[0]["xlabel"], "time[s]")
+        self.assertEqual(self.obj.graph[0]["ylim"],
+                         self.obj.default["graph"][0]["ylim"])
+        self.assertEqual(len(self.obj.graph[0]["plot"]), 2)
+        self.assertTrue("color" in self.obj.graph[0]["plot"][0])
+        self.assertEqual(self.obj.graph[0]["legend"]["loc"], "best")
+        self.assertIsNone(self.obj.graph[0]["legend"]["ncol"])
 
 
 if __name__ == '__main__':
