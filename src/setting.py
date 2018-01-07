9eb63bb611cdc01930fd7b020b898516371309f3
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-07 14:07:31 +0900

    add dispose and some fix

--- src/setting.py
+++ src/setting.py
@@ -4,7 +4,7 @@ import yaml
 
 class Setting:
     default = None
-    user = None
+    __user = None
 
     setting = None
     graph = list()
@@ -16,14 +16,20 @@ class Setting:
         self.__user_file_path = user_file_path
 
     def configure(self):
-        self.input_yaml()
+        self.__input_yaml()
 
         self.setting = copy.deepcopy(self.default)
         del self.setting["graph"]
-        self.__update_setting(self.user, self.setting)
+        self.__update_setting(self.__user, self.setting)
 
         self.__update_graph(copy.deepcopy(self.default["graph"][0]),
-                            self.user["graph"], self.graph)
+                            self.__user["graph"], self.graph)
+
+    def dispose(self):
+        self.default.clear()
+        self.__user.clear()
+        self.setting.clear()
+        self.graph.clear()
 
     def __update_setting(self, from_, to):
         for key in to:
@@ -33,36 +39,26 @@ class Setting:
                 else:
                     to[key] = from_[key]
 
-    @staticmethod
-    def __update_graph(default, from_, to):
+    def __update_graph(self, default, from_, to):
         for f in from_:
             d = dict()
             for key in default:
                 if key == "plot":
                     ll = list()
-                    for ff in f[key]:
-                        dd = dict()
-                        for key2 in default["plot"][0]:
-                            if key2 in ff:
-                                dd[key2] = ff[key2]
-                            else:
-                                dd[key2] = default[key][0][key2]
-                        ll.append(dd)
+                    self.__update_graph(default[key][0], f[key], ll)
                     d[key] = ll
                 elif key == "legend":
                     dd = dict()
                     for key2 in default["legend"]:
-                        if key2 in f[key]:
-                            dd[key2] = f[key][key2]
-                        else:
-                            dd[key2] = default[key][key2]
+                        dd[key2] = f[key][key2] if key2 in f[key] \
+                                                else default[key][key2]
                     d[key] = dd
                 else:
                     d[key] = f[key] if key in f else default[key]
             to.append(d)
 
-    def input_yaml(self):
+    def __input_yaml(self):
         with open(self.__default_file_path) as f:
             self.default = yaml.load(f)
         with open(self.__user_file_path) as f:
-            self.user = yaml.load(f)
+            self.__user = yaml.load(f)
