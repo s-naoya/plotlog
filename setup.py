9094f0ff9ad3dfe12edec067512aa2aeaa9e403b
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-01-27 15:09:02 +0900

    upload pypi

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
-import sys
-from cx_Freeze import setup, Executable
+from setuptools import setup, find_packages
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
 
-options = {
-    'build_exe': {
-        'packages': [
-            "pandas",
-            "numpy"
-        ],
-        'includes': [
-            'datacut',
-            'setting',
-            'plotgraph',
-            'matplotlib.backends.backend_tkagg',
-            'tkinter.filedialog'
-        ],
-        'path': sys.path + ["src"]
-    }
-}
+requires = ["pandas>=0.21.1", "matplotlib>=2.1.1", "PyYAML>=3.12"]
 
-base = 'Console'
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
 
-executables = [
-    Executable('plotlog.py', base=base)
-]
-
-setup(name='plotlog',
-      version='1.0.0',
-      description='Plot graph for many log file that is managed by DATE',
-      options=options,
-      executables=executables)
+setup(
+    name='plotlog',
+    version='0.1.2',
+    description='Plot graph for many log file that is managed by DATE',
+    long_description=readme,
+    url='https://github.com/s-naoya/plotlog',
+    author='SAITO Naoya',
+    author_email='saito3110.naoya@gmail.com',
+    license='MIT',
+    keywords='plot graph logfile',
+    packages=find_packages(),
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      plotlog = plotlog:main
+    """
+)
