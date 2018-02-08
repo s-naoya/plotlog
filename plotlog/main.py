a4be28d9f6fb58b0e802e832410a0cca574ef43b
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-02-09 03:42:50 +0900

    adjust selectlog

--- plotlog/main.py
+++ plotlog/main.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-import os
-import re
 import sys
-import copy
 import argparse
-from glob import glob
-from os.path import splitext, basename, isfile, isdir
+from os.path import isfile
 
 from plotlog.datacut import DataCut
 from plotlog.plotgraph import PlotGraph
@@ -17,71 +13,35 @@ def main():
     args = arg_parser()
 
     st = Setting(args.setting)
-    sl = SelectLog()
 
     if args.copy:
         copy_config(st.default_strings)
 
     st.configure()
 
-    log_file_paths = get_log_file_paths(args, st.setting)
+    sl = SelectLog()
+    log_file_paths = sl.get_logfile_paths(args, st.setting["put_log_dir"],
+                                          st.setting["graph_save_dir"],
+                                          st.setting["log_date_type"])
     pg = PlotGraph()
 
     for log_file_path in log_file_paths:
         data, memo = setup_data_frame(args, st.setting, log_file_path)
-        log_file_name = splitext(basename(log_file_path))[0]
+        logfile_name = sl.get_fn(log_file_path)
 
-        if is_fn_in_date(log_file_name, st.setting["log_date_type"]) is None:
-            save_dir = st.setting["graph_save_dir"] + "other/" + log_file_name
-            if not isdir(save_dir):
-                os.makedirs(save_dir)
-        else:
-            date_time, date = get_date(log_file_path,
-                                       st.setting["log_date_type"])
-            save_dir = st.setting["graph_save_dir"] + date + "/" + date_time
-            if not isdir(save_dir):
-                os.makedirs(save_dir)
+        save_dir = sl.setup_save_dir(logfile_name,
+                                     st.setting["log_date_type"],
+                                     st.setting["graph_save_dir"])
 
         for stg in st.graph:
-            graph_path = save_dir + "/" + log_file_name + "_" + stg["name"] \
+            graph_path = save_dir + "/" + logfile_name + "_" + stg["name"] \
                          + memo + "." + st.setting["graph_extension"]
             pg.plot(log_file_path, graph_path,
                     st.setting, stg, data.x_axis, data.df)
-        print("Complete", log_file_name)
+        print("Complete", logfile_name)
 
         data.dispose()
     st.dispose()
-
-
-def get_log_file_paths(args, st):
-    paths = []
-    if args.input:
-        paths = copy.copy(args.input)
-    elif args.all:
-        paths = glob(st["put_log_dir"]+"*")
-    elif args.after:
-        for path in glob(st["put_log_dir"] + "*"):
-            fn = splitext(basename(path))[0]
-            if fn >= args.after[0] and is_fn_in_date(fn, st["log_date_type"]):
-                paths.append(path)
-    elif args.select:
-        for date in args.select:
-            path = st["put_log_dir"] + date + "." + st["log_extension"]
-            paths.append(path)
-    elif args.new:
-        for path in glob(st["put_log_dir"] + "*"):
-            date_time, date = get_date(path, st["log_date_type"])
-            if not isdir(st["graph_save_dir"] + date + "/" + date_time):
-                paths.append(path)
-
-    for path in paths:
-        if not isfile(path):
-            print(path, "is not exits.")
-            paths.remove(path)
-    if len(paths) == 0:
-        print("There is no target log file.")
-        sys.exit()
-    return paths
 
 
 def setup_data_frame(args, st, path):
@@ -98,30 +58,6 @@ def setup_data_frame(args, st, path):
         st["xlim"] = [float(args.slice[0]), float(args.slice[1])]
         memo += "_slice_" + args.slice[0] + "_" + args.slice[1]
     return data, memo
-
-
-def get_date(path, f_type):
-    date_time = splitext(basename(path))[0]
-    date = date_time[0:6] if f_type == 0 or f_type == 1 else date_time[0:8]
-    return date_time, date
-
-
-def is_fn_in_date(s, f_type):
-    if f_type == 0:
-        return re.search("[%s]" % s, "[0-9]{2}[0-1][0-9][0-3][0-9]"
-                                     "[0-2][0-9][0-5][0-9][0-5][0-9]")
-    elif f_type == 1:
-        return re.search("[%s]" % s, "[0-9]{2}[0-1][0-9][0-3][0-9]"
-                                     "[0-2][0-9][0-5][0-9]")
-    elif f_type == 2:
-        return re.search("[%s]" % s, "[0-9]{4}[0-1][0-9][0-3][0-9]"
-                                     "[0-2][0-9][0-5][0-9][0-5][0-9]")
-    elif f_type == 3:
-        return re.search("[%s]" % s, "[0-9]{4}[0-1][0-9][0-3][0-9]"
-                                     "[0-2][0-9][0-5][0-9]")
-    else:
-        print("error: log_date_type is 0 or 1 or 2 or 3")
-        sys.exit()
 
 
 def copy_config(string):
