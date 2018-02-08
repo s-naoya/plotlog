377bb93f7efcd088efc62f80d80da6f37cb0b7b1
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2018-02-09 00:41:40 +0900

    add args.input

--- plotlog/selectlog.py
+++ plotlog/selectlog.py
@@ -1,3 +1,21 @@
+import copy
+
+
 class SelectLog:
     def __init__(self):
         pass
+
+    def get_logfile_paths(self, args, put_log_dir, graph_save_dir, log_date_type):
+        paths = list()
+        if args.input:
+            paths = copy.copy(args.input)
+        elif args.all:
+            pass
+        elif args.after:
+            pass
+        elif args.select:
+            pass
+        elif args.new:
+            pass
+
+        return paths
--- test/test_selectLog.py
+++ test/test_selectLog.py
@@ -1,13 +1,30 @@
 import unittest
 from plotlog.selectlog import SelectLog
+from argparse import Namespace
+import os.path
 
 
 class TestSelectLog(unittest.TestCase):
     def setUp(self):
-        self.obj = SelectLog()
+        self.sl = SelectLog()
+        self.put_log_dir = "log/"
+        self.graph_save_dir = "graph/"
+        self.log_date_type = "0"
+        self.args = Namespace(after=None, all=False, copy=False, input=None,
+                              new=True, noshift=False, select=None,
+                              setting='user.yml', slice=None)
 
-    def test_test(self):
-        self.assertEqual(0, 0)
+    def test_arg_input(self):
+        self.args = Namespace(after=None, all=False, copy=False,
+                              input=["log/170101000000.csv", "log/170101120000.csv"],
+                              new=True, noshift=False, select=None,
+                              setting='user.yml', slice=None)
+        paths = self.sl.get_logfile_paths(self.args, self.put_log_dir,
+                                          self.graph_save_dir, self.log_date_type)
+        self.assertEqual(paths[0], "log/170101000000.csv")
+        self.assertEqual(paths[1], "log/170101120000.csv")
+        self.assertTrue(os.path.exists(paths[0]))
+        self.assertTrue(os.path.exists(paths[1]))
 
 
 if __name__ == '__main__':
